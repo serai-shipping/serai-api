673a08b4e98ee38748312f40c04cf95a53aae920
serai-shipping <serai@seraishipping.com>
2022-05-07 08:46:43 +0400

    update

--- app.py
+++ app.py
@@ -2,25 +2,104 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from flask import Flask, request, jsonify
+from email.utils import formataddr
+from flask_mail import Mail
+from flask_mail import Message
 
 
 
-try:
-    port = 587  # For starttls
-    smtp_server = "smtp.gmail.com"
-    sender_email = "devrahul4798@gmail.com"
-    receiver_email = "devrahul4798@gmail.com"
-    password = input("Type your password and press enter:")
-    message = "Hello Test with python"
 
-    server = smtplib.SMTP(smtp_server,port)
-    server.ehlo() # Can be omitted
-    #server.starttls(context=context) # Secure the connection
-    server.ehlo() # Can be omitted
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-except Exception as e:
-        # Print any error messages to stdout
-        print(e)
-finally:
-        server.quit() 
+app = Flask(__name__) 
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'devrahul4798@gmail.com'
+app.config['MAIL_PASSWORD'] = 'rahkavlit'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
+
+@app.route('/', methods=['GET'])  
+def index(): 
+    return "<h1>Serai Shipping API</h1>"
+
+@app.route('/send-email', methods=['GET', 'POST'])
+def respond():
+    msg = Message('Hello Test Mail', sender =   'devrahul4798@gmail.com', recipients = ['devrahul4798@gmail.com'])
+    msg.body = "TEST MAIL CHECK"
+    mail.send(msg)
+    return "Message sent!"
+
+    # # data = request.get_json()
+    # # port = 587  # For starttls
+    # # smtp_server = "smtp.gmail.com"
+    # # sender_email = "devrahul4798@gmail.com"
+    # # receiver_email = "devrahul4798@gmail.com"
+    # # password = "rahkavlit"
+    # # name = data['name']
+    # # email = data['email']
+    # # contact = data['contact']
+    # # content = data['data']
+
+    # # # Create a secure SSL context
+    # # context = ssl.create_default_context()
+
+    # # message = MIMEMultipart("alternative")
+    # # message["Subject"] = "Business Enquiry - Serai Shipping Services"
+    # # message["From"] = formataddr(('Serai Shipping Business', 'serai@seraishipping.com'))
+    # # message["To"] = receiver_email
+    # # # message["Name"] = name
+
+    # # # Create the plain-text and HTML version of your message
+    # # text = f"""\
+    # # Hi Team,
+    # # There's a business enquiry for you! 
+    
+    # # Please find the enquiry details as mentioned below: 
+
+    # # Name - {name}
+
+    # # Email - {email} 
+
+    # # Contact Number - {contact}
+    
+    # # Message - {content}
+    
+    # # Have a great day ahead!
+    # # """
+    # # html = f"""\
+    # # <html>
+    # # <body>
+    # #     <p>Hi Team, <br>
+    # #     There's a business enquiry for you from {name} with email - {email} and contact {contact} with message {message}<br>
+    # #     </p>
+    # # </body>
+    # # </html>
+    # """
+
+    # Turn these into plain/html MIMEText objects
+    # part1 = MIMEText(text, "plain")
+    # # part2 = MIMEText(html, "html")
+
+    # # Add HTML/plain-text parts to MIMEMultipart message
+    # # The email client will try to render the last part first
+    # message.attach(part1)
+    # # message.attach(part2)
+
+    # # Create secure connection with server and send email
+    # context = ssl.create_default_context()
+    # with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    #     server.login(sender_email, password)
+    #     server.sendmail(
+    #         sender_email, receiver_email, message.as_string()
+    #     )
+
+
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
+
+# # Try to log in to server and send email
+
