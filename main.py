754f8597c33031586a38e3f51b52f96b2941d77b
serai-shipping <serai@seraishipping.com>
2022-05-07 08:14:15 +0400

    update

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from flask import Flask, request, jsonify
 from email.utils import formataddr
+from flask_mail import Mail
+from flask_mail import Message
+
+
 
 
 app = Flask(__name__) 
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'devrahul4798@gmail.com'
+app.config['MAIL_PASSWORD'] = 'rahkavlit'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
 
 @app.route('/', methods=['GET'])  
 def index(): 
@@ -13,69 +25,74 @@ def index():
 
 @app.route('/send-email', methods=['GET', 'POST'])
 def respond():
-    data = request.get_json()
-    port = 587  # For starttls
-    smtp_server = "smtp.gmail.com"
-    sender_email = "devrahul4798@gmail.com"
-    receiver_email = "devrahul4798@gmail.com"
-    password = "rahkavlit"
-    name = data['name']
-    email = data['email']
-    contact = data['contact']
-    content = data['data']
+    msg = Message('Hello from the other side!', sender =   'devrahul4798@gmail.com', recipients = ['devrahul4798@gmail.com'])
+    msg.body = "TEST MAIL CHECK"
+    mail.send(msg)
+    return "Message sent!"
 
-    # Create a secure SSL context
-    context = ssl.create_default_context()
+    # # data = request.get_json()
+    # # port = 587  # For starttls
+    # # smtp_server = "smtp.gmail.com"
+    # # sender_email = "devrahul4798@gmail.com"
+    # # receiver_email = "devrahul4798@gmail.com"
+    # # password = "rahkavlit"
+    # # name = data['name']
+    # # email = data['email']
+    # # contact = data['contact']
+    # # content = data['data']
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "Business Enquiry - Serai Shipping Services"
-    message["From"] = formataddr(('Serai Shipping Business', 'serai@seraishipping.com'))
-    message["To"] = receiver_email
-    # message["Name"] = name
+    # # # Create a secure SSL context
+    # # context = ssl.create_default_context()
 
-    # Create the plain-text and HTML version of your message
-    text = f"""\
-    Hi Team,
-    There's a business enquiry for you! 
+    # # message = MIMEMultipart("alternative")
+    # # message["Subject"] = "Business Enquiry - Serai Shipping Services"
+    # # message["From"] = formataddr(('Serai Shipping Business', 'serai@seraishipping.com'))
+    # # message["To"] = receiver_email
+    # # # message["Name"] = name
+
+    # # # Create the plain-text and HTML version of your message
+    # # text = f"""\
+    # # Hi Team,
+    # # There's a business enquiry for you! 
     
-    Please find the enquiry details as mentioned below: 
+    # # Please find the enquiry details as mentioned below: 
 
-    Name - {name}
+    # # Name - {name}
 
-    Email - {email} 
+    # # Email - {email} 
 
-    Contact Number - {contact}
+    # # Contact Number - {contact}
     
-    Message - {content}
+    # # Message - {content}
     
-    Have a great day ahead!
-    """
-    html = f"""\
-    <html>
-    <body>
-        <p>Hi Team, <br>
-        There's a business enquiry for you from {name} with email - {email} and contact {contact} with message {message}<br>
-        </p>
-    </body>
-    </html>
-    """
+    # # Have a great day ahead!
+    # # """
+    # # html = f"""\
+    # # <html>
+    # # <body>
+    # #     <p>Hi Team, <br>
+    # #     There's a business enquiry for you from {name} with email - {email} and contact {contact} with message {message}<br>
+    # #     </p>
+    # # </body>
+    # # </html>
+    # """
 
     # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
-    # part2 = MIMEText(html, "html")
+    # part1 = MIMEText(text, "plain")
+    # # part2 = MIMEText(html, "html")
 
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part1)
-    # message.attach(part2)
+    # # Add HTML/plain-text parts to MIMEMultipart message
+    # # The email client will try to render the last part first
+    # message.attach(part1)
+    # # message.attach(part2)
 
-    # Create secure connection with server and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(
-            sender_email, receiver_email, message.as_string()
-        )
+    # # Create secure connection with server and send email
+    # context = ssl.create_default_context()
+    # with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    #     server.login(sender_email, password)
+    #     server.sendmail(
+    #         sender_email, receiver_email, message.as_string()
+    #     )
 
 
 
