076f10e0ebf2d0b0c94788edf45f9b88d464d7ba
serai-shipping <serai@seraishipping.com>
2022-05-07 06:06:58 +0400

    update

--- main.py
+++ main.py
@@ -7,28 +7,32 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])  
 def index(): 
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
+    return "<h1>Serai Shipping API</h1>"
 
 @app.route('/send-email', methods=['GET'])
 def respond():
     response = {status: working}
     try:
-        server = smtplib.SMTP(smtp_server,port)
-        server.ehlo() # Can be omitted
-        server.starttls(context=context) # Secure the connection
-        server.ehlo() # Can be omitted
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-       #Send email here
+       port = 587  # For starttls
+       smtp_server = "smtpout.secureserver.net"
+       sender_email = "serai@seraishipping.com"
+       receiver_email = "serai@seraishipping.com"
+       password = input("Type your password and press enter:")
+       message = "Hello Test with python"
+       Subject: "TEST"
+
+       server = smtplib.SMTP(smtp_server,port)
+       server.ehlo() # Can be omitted
+       server.starttls(context=context) # Secure the connection
+       server.ehlo() # Can be omitted
+       server.login(sender_email, password)
+       server.sendmail(sender_email, receiver_email, message)
     except Exception as e:
         # Print any error messages to stdout
         print(e)
     finally:
         server.quit() 
     return jsonify(response)
-
-
-
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
